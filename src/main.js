901901f98c3ce2b828d109621237ec169c7738db
DaHong123 <18856966603@163.com>
2020-11-17 15:38:59 +0800

    项目初始化完成-调整目录结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建vue根实例
+// 把router配置到根实例中
 new Vue({
   router,
   render: h => h(App)
